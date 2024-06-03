271947b1447ff879d6894a3abc5332aef1dcc480
Rdani2005 <daniel.sequeira@betanysports.eu>
2024-06-03 07:40:28 -0600

    feature - accordion component

---
+++ src/app/dashboard/accordion/page.tsx
@@ -0,0 +1,42 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import React from "react";
+
+const items = [
+  {
+    id: "item-1",
+    question: "Is it accessible?",
+    answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    id: "item-2",
+    question: "How do I get Started?",
+    answer: "You can start by reading the documentation.",
+  },
+  {
+    id: "item-3",
+    question: "Can I use it on multiple projects?",
+    answer: "Yes. It's licensed under the MIT license.",
+  },
+];
+
+const AccordionPage = () => {
+  return (
+    <div>
+      <Accordion type="single" collapsible className="w-full">
+        {items.map((item) => (
+          <AccordionItem value={item.id} key={item.id}>
+            <AccordionTrigger>{item.question}</AccordionTrigger>
+            <AccordionContent>{item.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
+};
+
+export default AccordionPage;
---
+++ src/app/dashboard/alert/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AlertPage = () => {
+  return (
+    <div>
+      <h1>Alert Page</h1>
+    </div>
+  );
+};
+
+export default AlertPage;
